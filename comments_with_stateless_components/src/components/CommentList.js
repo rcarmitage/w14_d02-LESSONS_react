68617572e16e2c9e64160d75e974d954f447f163
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-03 23:39:06 +0100

    create-react-app uk_top_20

--- comments_with_stateless_components/src/components/CommentList.js
+++ comments_with_stateless_components/src/components/CommentList.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Comment from './Comment';
 
 const CommentList = ({data}) => {
-    const commentNodes = data.map(comment => {
-      return (
-        <Comment author={comment.author} key={comment.id}>
-          {comment.text}
-        </Comment>
-      );
-    });
-    
+  const commentNodes = data.map(comment => {
     return (
-      <div className="comment-list">
-        {commentNodes}
-      </div>
-    )
-  }
+      <Comment author={comment.author} key={comment.id}>
+        {comment.text}
+      </Comment>
+    );
+  });
+  
+  return (
+    <div className="comment-list">
+      {commentNodes}
+    </div>
+  )
+}
 
 export default CommentList;
