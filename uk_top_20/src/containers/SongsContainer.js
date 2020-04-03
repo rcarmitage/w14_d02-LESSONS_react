2a1b1f195d763b626062b3fd3ab07adeec5039d6
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-04 00:36:26 +0100

    trying to get songs rendered, not working yet

--- uk_top_20/src/containers/SongsContainer.js
+++ uk_top_20/src/containers/SongsContainer.js
@@ -10,12 +10,22 @@ class SongsContainer extends React.Component {
     };
   }
 
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+
+    fetch(url)
+      .then(res => res.json())
+      .then(songs => this.setState({songs: songs}))
+      .catch(err => console.error);
+  }
+
   render() {
     return (
       <div>
-        <h4>Songs list goes here</h4>
+        <h4>UK Top 40</h4>
+        <SongsList songs={this.state.songs} />
       </div>
-    )
+    );
   }
 }
 
